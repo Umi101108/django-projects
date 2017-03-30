bb74eb15a1e63f9c8aa308d0c44494b59beabcdb
wangwei <408465808@qq.com>
2017-03-30 19:05:14 +0800

    helloworld

---
+++ HelloWorld/HelloWorld/testdb.py
@@ -0,0 +1,36 @@
+# coding:utf8
+from django.http import HttpResponse
+from TestModel.models import Test
+
+def testdb(request):
+    # test1 = Test(name='runoob27')
+    # test1.save()
+    # return HttpResponse("<p>数据添加成功</p>")
+
+
+    response = ""
+    response1 = ""
+
+    list = Test.objects.all()
+
+    response2 = Test.objects.filter(id = 1)
+
+    response3 = Test.objects.get(id = 1)
+
+    Test.objects.order_by('name')[0:2]
+
+    Test.objects.order_by("id")
+
+    Test.objects.filter(name="runoob").order_by("id")
+
+    for var in list:
+        response1 += var.name + " "
+
+    response = response1
+    return HttpResponse("<p>" + response3.name + "</p>")
+
+    # test1 = Test.objects.get(id=1)
+    # test1.name = 'Google'
+    # test1.save()
+
+    # return HttpResponse("<p>修改成功</p>")
