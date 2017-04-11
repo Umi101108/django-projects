4d0811e810bbb9454713e13c95c9b39212b80399
Umi101108 <408465808@qq.com>
2017-04-11 17:21:21 +0800

    tag
    add tag

--- example_site/blog/models.py
+++ example_site/blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -31,9 +32,26 @@ class Post(models.Model):
     def __unicode__(self):
         return self.title
 
-    object = models.Manager()
+    objects = models.Manager()
     published = PublishedManager()
+
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), self.slug])
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created', )
+
+    def __unicode__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
--- example_site/blog/views.py
+++ example_site/blog/views.py
@@ -1,13 +1,20 @@
 # coding: utf8
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 # Create your views here.
-def post_list(request):
+def post_list(request, tag_slug=None):
 	object_list = Post.published.all()
+	tag = None
+	if tag_slug:
+		tag = get_object_or_404(Tag, slug=tag_slug)
+		object_list = object_list.filter(tags__in=[tag])
+
 	paginator = Paginator(object_list, 3)
 	page = request.GET.get('page')
 	try:
@@ -16,13 +23,27 @@ def post_list(request):
 		posts = paginator.page(1)
 	except EmptyPage:
 		posts = paginator.page(paginator.num_pages)
-	return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+	return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 	# posts = Post.published.all()
 	# return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-	return render(request, 'blog/post/detail.html', {'post': post})
+	comments = post.comments.filter(active=True)
+	new_comment = None
+	if request.method == 'POST':
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+			new_comment = comment_form.save(commit=False)
+			new_comment.post = post
+			new_comment.save()
+	else:
+		comment_form = CommentForm()
+
+	post_tags_ids = post.tags.values_list('id', flat=True)
+	similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+	similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+	return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form, 'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
@@ -46,3 +67,4 @@ def post_share(request, post_id):
 	else:
 		form = EmailPostForm()
 	return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
