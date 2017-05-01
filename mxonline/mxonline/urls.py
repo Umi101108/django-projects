7fc3a8d499493d4475b5f2c0ebc28c32cd7cc620
wangwei <408465808@qq.com>
2017-05-01 23:35:19 +0800

    “”

--- mxonline/mxonline/urls.py
+++ mxonline/mxonline/urls.py
@@ -21,9 +21,11 @@ import xadmin
 
 from users.views import login
 
+
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="index.html"), name="index"),
-    url(r'^login/$', TemplateView.as_view(template_name="login.html"), name="login"),
+    # url(r'^login/$', TemplateView.as_view(template_name="login.html"), name="login"),
+    url(r'^login/$', login, name="login"),
 ]
